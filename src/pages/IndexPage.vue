2670ca3e100aa6bca9f9bd574754840fbd339045
Nikita Kurbatow <mtobi3911@gmail.com>
2024-10-09 09:43:55 +0300

    1

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>f</h1>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+
+function onItemClick (evt) {
+  console.log(evt)
+}
+
+
+
+</script>
+
+<script>
+
+</script>
+
