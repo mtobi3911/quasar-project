f6e7d43436623273155d5ab575a612ca2dacd3fe
Nikita Kurbatow <mtobi3911@gmail.com>
2024-10-11 10:20:26 +0300

    Updates

--- assets/IndexPage.16016de9.js
+++
@@ -1,1 +0,0 @@
-import{W as e,X as a}from"./index.3e60a2c9.js";const s=Object.assign({name:"IndexPage"},{__name:"IndexPage",setup(n){return(t,c)=>(e(),a("h1",null,"f"))}});export{s as default};
---
+++ assets/IndexPage.568072c9.js
@@ -0,0 +1,1 @@
+import{W as e,X as a}from"./index.364d103c.js";const s=Object.assign({name:"IndexPage"},{__name:"IndexPage",setup(n){return(t,c)=>(e(),a("h1",null,"f"))}});export{s as default};
