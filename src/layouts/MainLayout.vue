8a9c74e49c83829fbdd05dd9796de8959bc82274
Nikita Kurbatow <mtobi3911@gmail.com>
2024-10-11 11:19:20 +0300

    Исправления

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -136,13 +136,6 @@ const incomingMails = ref([
 
 
 // Функция для отправки данных с проверкой
-// const handleSubmit = () => {
-//   if (email_adres.value.trim() === '') {
-//     emailError.value = true
-//     errorMessage.value = 'Введите значение'
-//   } else {
-//     emailError.value = false
-//     errorMessage.value = ''
 
 const handleSubmit = () => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
